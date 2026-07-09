075dea3615d33ca8ee308eefdab0fb6ef1457d38
ynsukim <yoon.kim.kr@gmail.com>
2026-07-09 22:18:58 +0900

    Update Supabase configuration with valid URL and anon key

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.SOLLINK_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://hwoidqyjjlzdmmhrkmap.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3b2lkcXlqamx6ZG1taHJrbWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1OTM3MzksImV4cCI6MjA5OTE2OTczOX0._926pKM4XmH2xnRBIvRuiZYd5jXv8P5xzxD-Zbg_-2A',
 };
