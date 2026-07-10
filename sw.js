318b659bf10b58bf88ce4936e0bbaf7a83f6417a
ynsukim <yoon.kim.kr@gmail.com>
2026-07-10 09:50:14 +0900

    stronger shake  view

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sollink-ab-v22';
+const CACHE = 'sollink-ab-v24';
 const ASSETS = ['./', './index.html', './css/styles.css', './js/config.js', './js/analytics.js', './js/app.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
